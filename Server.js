457ba018a07ff1fc4125d3553f9994980c1ca5ef
blastwacia <ciatotoappdana@gmail.com>
2024-12-13 21:14:19 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,25 +1,17 @@
+// Backend - app.js
 const express = require('express');
-const multer = require('multer');
 const { Client } = require('whatsapp-web.js');
-const path = require('path');
 const http = require('http');
+const path = require('path');
 const socketIo = require('socket.io');
 
-// Setup Express dan HTTP server
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server); // Menghubungkan socket.io ke server
+const io = socketIo(server);
 
-const port = 4000;
-
-// Middleware untuk parsing data
-app.use(express.static(path.join(__dirname, 'public')));  // Untuk menyajikan file statis seperti HTML
-
-// Setup WhatsApp client
 const client = new Client();
 
 client.on('qr', (qr) => {
-    // Kirim QR Code ke frontend menggunakan socket.io
     io.emit('qr', qr);
     console.log('QR RECEIVED');
 });
@@ -31,12 +23,13 @@ client.on('ready', () => {
 
 client.initialize();
 
-// Halaman utama untuk upload CSV dan template pesan
+app.use(express.static(path.join(__dirname, 'public'))); // serve static files from 'public' directory
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));  // Memastikan file HTML dikelola oleh server
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // serve your HTML file
 });
 
-// Mulai server
+const port = 4000;
 server.listen(port, () => {
-    console.log(`Server berjalan di http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
